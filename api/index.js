dacd04e55ccb575f8e5cc550a0820daf6e57c7b5
MinHiuLe <lmhieu0204@gmail.com>
2025-03-08 14:14:09 +0700

    add port 8080

--- api/index.js
+++ api/index.js
@@ -174,7 +174,7 @@ io.on('connection', async (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001 || 8080;
 if (process.env.NODE_ENV !== 'production') {
   server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
