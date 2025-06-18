ab845265b724bdd99a7be4a19bee406d233b451d
Hieu Le <124018367+MinHiuLe@users.noreply.github.com>
2025-06-18 21:32:41 +0700

    Add files via upload

---
+++ api/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, minlength: 5, maxlength: 20 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullName: { type: String },
+  dateOfBirth: { type: Date },
+  gender: { type: String, enum: ['male', 'female', 'other'] },
+  phone: { type: String },
+});
+
+userSchema.index({ username: 1, email: 1 }); 
+
+module.exports = mongoose.model('User', userSchema);
